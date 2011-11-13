22c3f4a0d3b2e06614b163ad4110c60baf117a75
Peter Cheng <pc@agilesource.org>
2011-11-13 11:48:50 +0800

    update license

--- src/main/java/org/teaframework/services/datasource/ContainerDataSourceServiceImpl.java
+++ src/main/java/org/teaframework/services/datasource/ContainerDataSourceServiceImpl.java
@@ -16,6 +16,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
  * See the License for the specific language governing permissions and 
  * limitations under the License. 
+ * 
+ * For more information, please visit:
+ * http://github.com/agilesource/teaframework
  */
 
 package org.teaframework.services.datasource;
